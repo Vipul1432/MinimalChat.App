ddc9b5f476471026b901d855d46d619e2e2f25f0
Vipul143212 <vipulu563@gmail.com>
2023-09-27 12:04:55 +0530

    Implement real time chat with SignalR

--- src/app/_shared/services/chat.service.ts
+++ src/app/_shared/services/chat.service.ts
@@ -3,77 +3,123 @@ import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../environments/environment';
 import { UserChat } from '../models/UserChat';
-import { Log } from '../models/Log';
-import { format } from 'date-fns';
+import * as signalR from '@microsoft/signalr';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ChatService {
   private apiUrl = environment.apiUrl;
+  hubConnection!: signalR.HubConnection;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+    if (
+      !this.hubConnection ||
+      this.hubConnection.state === signalR.HubConnectionState.Disconnected
+    ) {
+      this.hubConnection = new signalR.HubConnectionBuilder()
+        .withUrl('https://localhost:7038/chatHub')
+        .withAutomaticReconnect()
+        .build();
+      this.startSignalRConnection();
+    }
+  }
+
+  // Check if the HubConnection is in the 'Disconnected' state.
+  isConnectionDisconnected(): boolean {
+    return this.hubConnection.state === signalR.HubConnectionState.Disconnected;
+  }
 
   /**
- * Retrieves a JSON Web Token (JWT) from local storage.
- *
- * @returns The JWT token as a string if it's stored in local storage, or null if not found.
- */
+   * Initiates a connection to the SignalR hub for real-time communication.
+   * Logs the success or failure of the SignalR connection attempt.
+   */
+  private startSignalRConnection(): void {
+    this.hubConnection
+      .start()
+      .then(() => {
+        console.log('SignalR connection started');
+      })
+      .catch((err) => {
+        console.error('Error while starting SignalR connection: ' + err);
+      });
+  }
+
+  /**
+   * Initiates a connection to the WebSocket hub for real-time communication.
+   * Logs the success or failure of the connection attempt.
+   */
+  startConnection(): void {
+    this.hubConnection
+      .start()
+      .then(() => {
+        console.log('Connection started');
+      })
+      .catch((err) => {
+        console.error('Error while starting connection: ' + err);
+      });
+  }
+
+  /**
+   * Retrieves a JSON Web Token (JWT) from local storage.
+   *
+   * @returns The JWT token as a string if it's stored in local storage, or null if not found.
+   */
   getToken(): string | null {
     return localStorage.getItem('token');
   }
 
   /**
- * Creates and returns HttpHeaders with the necessary authentication headers.
- *
- * @returns HttpHeaders object with "Content-Type" and "Authorization" headers.
- */
+   * Creates and returns HttpHeaders with the necessary authentication headers.
+   *
+   * @returns HttpHeaders object with "Content-Type" and "Authorization" headers.
+   */
   private getHeaders(): HttpHeaders {
     const token = this.getToken();
     return new HttpHeaders({
       'Content-Type': 'application/json',
-      'Authorization': `Bearer ${token}`
+      Authorization: `Bearer ${token}`,
     });
   }
 
   /**
- * Retrieves user chat messages from the API based on specified parameters.
- *
- * @param UserId - The ID of the user for whom to retrieve chat messages.
- * @param Before - Optional. A date used to filter messages before a certain date.
- * @param count - Optional. The maximum number of messages to retrieve.
- * @param sortOrder - Optional. The sort order of the messages.
- * @returns An Observable containing an array of UserChat objects representing chat messages.
- */
-  getUserChat( 
+   * Retrieves user chat messages from the API based on specified parameters.
+   *
+   * @param UserId - The ID of the user for whom to retrieve chat messages.
+   * @param Before - Optional. A date used to filter messages before a certain date.
+   * @param count - Optional. The maximum number of messages to retrieve.
+   * @param sortOrder - Optional. The sort order of the messages.
+   * @returns An Observable containing an array of UserChat objects representing chat messages.
+   */
+  getUserChat(
     UserId: string,
     Before: Date | null,
     count: number | null,
     sortOrder: number | null
-    ): Observable<UserChat[]> {
+  ): Observable<UserChat[]> {
     const url = `${this.apiUrl}messages`;
     const headers = this.getHeaders();
     let params = new HttpParams().set('UserId', UserId);
-      if (Before !== null) {
-        params = params.set('before', Before.toString());
-      }  
-      if (count !== null) {
-        params = params.set('count', count.toString());
-      }
-      if (sortOrder !== null) {
-        params = params.set('sortOrder', sortOrder.toString());
-      }
+    if (Before !== null) {
+      params = params.set('before', Before.toString());
+    }
+    if (count !== null) {
+      params = params.set('count', count.toString());
+    }
+    if (sortOrder !== null) {
+      params = params.set('sortOrder', sortOrder.toString());
+    }
     return this.http.get<UserChat[]>(url, { headers, params });
   }
 
   /**
- * Sends a chat message to a specified receiver via a POST request to the API.
- *
- * @param receiverId - The ID of the message receiver.
- * @param content - The content of the chat message.
- * @returns An Observable representing the result of the message sending process.
- *          Subscribers can handle the response asynchronously.
- */
+   * Sends a chat message to a specified receiver via a POST request to the API.
+   *
+   * @param receiverId - The ID of the message receiver.
+   * @param content - The content of the chat message.
+   * @returns An Observable representing the result of the message sending process.
+   *          Subscribers can handle the response asynchronously.
+   */
   sendMessage(receiverId: string, content: string): Observable<any> {
     const url = `${this.apiUrl}messages`;
     const headers = this.getHeaders();
@@ -86,28 +132,28 @@ export class ChatService {
   }
 
   /**
- * Updates the content of a chat message via a PUT request to the API.
- *
- * @param messageId - The ID of the message to be updated.
- * @param content - The new content for the chat message.
- * @returns An Observable representing the result of the message update process.
- *          Subscribers can handle the response asynchronously.
- */
+   * Updates the content of a chat message via a PUT request to the API.
+   *
+   * @param messageId - The ID of the message to be updated.
+   * @param content - The new content for the chat message.
+   * @returns An Observable representing the result of the message update process.
+   *          Subscribers can handle the response asynchronously.
+   */
   updateMessageContent(messageId: number, content: string): Observable<any> {
     const url = `${this.apiUrl}messages/${messageId}`;
     const headers = this.getHeaders();
     const body = { content };
-  
+
     return this.http.put(url, body, { headers });
   }
 
   /**
- * Deletes a chat message via a DELETE request to the API.
- *
- * @param messageId - The ID of the message to be deleted.
- * @returns An Observable representing the result of the message deletion process.
- *          Subscribers can handle the response asynchronously.
- */
+   * Deletes a chat message via a DELETE request to the API.
+   *
+   * @param messageId - The ID of the message to be deleted.
+   * @returns An Observable representing the result of the message deletion process.
+   *          Subscribers can handle the response asynchronously.
+   */
   deleteMessage(messageId: number): Observable<any> {
     const url = `${this.apiUrl}messages/${messageId}`;
     const headers = this.getHeaders();
@@ -116,13 +162,13 @@ export class ChatService {
   }
 
   /**
- * Retrieves log data from the API within a specified time range.
- *
- * @param startTime - Optional. The start time for filtering log entries.
- * @param endTime - Optional. The end time for filtering log entries.
- * @returns An Observable containing log data based on the specified time range.
- *          Subscribers can handle the response asynchronously.
- */
+   * Retrieves log data from the API within a specified time range.
+   *
+   * @param startTime - Optional. The start time for filtering log entries.
+   * @param endTime - Optional. The end time for filtering log entries.
+   * @returns An Observable containing log data based on the specified time range.
+   *          Subscribers can handle the response asynchronously.
+   */
   fetchLogs(startTime?: Date | null, endTime?: Date | null): Observable<any> {
     const url = `${this.apiUrl}log`;
     const headers = new HttpHeaders({
@@ -144,11 +190,11 @@ export class ChatService {
   }
 
   /**
- * Formats a JavaScript Date object into a string representation with a specific format.
- *
- * @param date - The Date object to be formatted.
- * @returns A formatted string representation of the date and time.
- */
+   * Formats a JavaScript Date object into a string representation with a specific format.
+   *
+   * @param date - The Date object to be formatted.
+   * @returns A formatted string representation of the date and time.
+   */
   private formatDate(date: Date): string {
     const year = date.getFullYear();
     const month = String(date.getMonth() + 1).padStart(2, '0');
@@ -157,8 +203,7 @@ export class ChatService {
     const minutes = String(date.getMinutes()).padStart(2, '0');
     const seconds = String(date.getSeconds()).padStart(2, '0');
     const milliseconds = String(date.getMilliseconds()).padStart(3, '0');
-  
+
     return `${year}-${month}-${day}T${hours}:${minutes}:${seconds}.${milliseconds}Z`;
   }
 }
-  
